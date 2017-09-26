ebc833da823ae011f8606d2169d78a062221a3fb
rajmaravanthe <rraajjpoojary@gmail.com>
2017-09-26 16:09:27 +0530

    added file

---
+++ person.js
@@ -0,0 +1,10 @@
+(function(){
+    function Person (name, age, city) {
+        console.log('name: '+ name);
+        console.log('age: '+ age);
+        console.log('city: '+ city);
+    }
+
+    val1 = new Person("rajesh",29,"Bangalore");
+    val2 = new Person("Santhosg",31,"Mumbai");
+})()
